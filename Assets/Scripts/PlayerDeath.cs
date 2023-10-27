ce01c371bafff828b7da87448167a178f064c63b
Bekir Can Ata <bekircnata@gmail.com>
2023-10-27 21:47:15 +0300

    Death mekanigi eklendi

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private Animator anim;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip deathSoundEffect;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            anim.SetTrigger("death");
+            audioSource.clip = deathSoundEffect;
+            audioSource.Play();
+        }
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
