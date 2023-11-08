c905c0a3ef387f8f6493131b6387d59f9cdb3a9c
Bekir Can Ata <bekircnata@gmail.com>
2023-11-08 20:23:20 +0300

    Moving platform mekanigi eklendi

---
+++ Assets/Scripts/StickyPlatform.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerMovement>().isItOnTheGround = true;
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerMovement>().isItOnTheGround = false;
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
